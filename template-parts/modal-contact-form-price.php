192cbef5b52306206e601d0811a87aca8cd665fd
Slava Sidorov <sidorov-vv3@mail.ru>
2025-11-25 22:45:25 +0500

    Отправить сообщение - > Оставить заявку

--- template-parts/modal-contact-form-price.php
+++ template-parts/modal-contact-form-price.php
@@ -25,7 +25,7 @@
             <!-- Заголовок -->
             <div class="modal-header border-0">
                 <h5 class="modal-title text-center text-lg-start" id="contactModalLabel">
-                    Отправить сообщение
+                    Оставить заявку
                 </h5>
             </div>
 
