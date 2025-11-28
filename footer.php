1c83158b92f3776dcf88726ea7bd88e77d2a62a2
Slava Sidorov <sidorov-vv3@mail.ru>
2025-11-28 16:15:54 +0500

    чтобы не прыгало

--- footer.php
+++ footer.php
@@ -294,16 +294,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 </script>
-
-<?php
-if ( ! is_single() ) {
-    echo '<style>
-        .selected-services-list {
-            height: 50px !important;
-        }
-    </style>';
-}
-?>
 </body>
 
 
